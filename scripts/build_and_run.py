6b0e8b784bdce72fe116b8324576525ef61f6dd3
Eric Schade <eric.schade@yahoo.com>
2023-08-14 03:27:51 -0700

    update terminal output

--- scripts/build_and_run.py
+++ scripts/build_and_run.py
@@ -4,7 +4,7 @@ import os
 def compose():
     os.system('echo "Building and running Eikon backend challenge ETL..."')
     os.system("docker compose up -d")
-    os.system('echo "Service is running. Try triggering an ETL process with `python trigger_etl.py`"')
+    os.system('echo "Service is running. Try triggering an ETL process with `python scripts/trigger_etl.py`"')
 
 
 if __name__ == "__main__":
